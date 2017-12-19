51ae20b5ec4325d4aaabb9f3cf0b398e564a36c9
TeoSocs <teo.sovi@gmail.com>
2017-12-19 15:40:47 +0100

    created artifacts

--- chapters/code/hlfvoting/voting/generate.sh
+++ chapters/code/hlfvoting/voting/generate.sh
@@ -1,4 +1,4 @@
-cryptogen generate --config=./crypto-config.yaml
-export FABRIC_CFG_PATH=$PWD
-configtxgen -profile VotingOrdererGenesis -outputBlock ./voting-genesis.block
-configtxgen -profile VotingChannel -outputCreateChannelTx ./voting-channel.tx -channelID votingchannel
+../../bin/cryptogen generate --config=./crypto-config.yaml
+export FABRIC_CFG_PATH=$PWD/
+../../bin/configtxgen -profile VotingOrdererGenesis -outputBlock ./voting-genesis.block
+../../bin/configtxgen -profile VotingChannel -outputCreateChannelTx ./voting-channel.tx -channelID votingchannel

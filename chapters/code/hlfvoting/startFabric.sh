1b5d32d90c52e0f5390bef5aedfbb348b5a3d178
Matteo Sovilla <teo.sovi@gmail.com>
2017-12-19 14:57:22 +0100

    typos

--- chapters/code/hlfvoting/startFabric.sh
+++ chapters/code/hlfvoting/startFabric.sh
@@ -22,5 +22,5 @@ sleep ${FABRIC_START_TIMEOUT}
 # Create the channel
 docker exec peer0.Stato.eurasia.com peer channel create -o orderer.eurasia.com:7050 -c votingchannel -f /etc/hyperledger/configtx/voting-channel.tx
 
-# Join peer0.stato.eurasia.com to the channel.
-docker exec -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@stato.eurasia.com/msp" peer0.Stato.eurasia.com peer channel join -b votingchannel.block
+# Join peer0.Stato.eurasia.com to the channel.
+docker exec -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@Stato.eurasia.com/msp" peer0.Stato.eurasia.com peer channel join -b votingchannel.block

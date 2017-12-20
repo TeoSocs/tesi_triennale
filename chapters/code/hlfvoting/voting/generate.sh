03a542c584f5a56484fae31eb1d98b353bc5f033
TeoSocs <teo.sovi@gmail.com>
2017-12-20 09:25:42 +0100

    crypto art regenerated

--- chapters/code/hlfvoting/voting/generate.sh
+++ chapters/code/hlfvoting/voting/generate.sh
@@ -1,4 +1,17 @@
-../../bin/cryptogen generate --config=./crypto-config.yaml
+#!/bin/bash
+
+# Exit on first error
+set -e
+# Grab the current directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+rm -rf crypto-config voting-genesis.block voting-channel.tx
+
+ls -laG
+
+${DIR}/../../bin/cryptogen generate --config=${DIR}/crypto-config.yaml
 export FABRIC_CFG_PATH=$PWD/
-../../bin/configtxgen -profile VotingOrdererGenesis -outputBlock ./voting-genesis.block
-../../bin/configtxgen -profile VotingChannel -outputCreateChannelTx ./voting-channel.tx -channelID votingchannel
+${DIR}/../../bin/configtxgen -profile VotingOrdererGenesis -outputBlock ${DIR}/voting-genesis.block
+${DIR}/../../bin/configtxgen -profile VotingChannel -outputCreateChannelTx ${DIR}/voting-channel.tx -channelID votingchannel
+
+ls -laG
